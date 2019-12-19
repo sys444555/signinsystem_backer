9ab95acf8eaf9445e3dbb9960e2d0707c9570005
sys444555 <369039285@qq.com>
2019-12-19 18:17:47 +0800

    。。

---
+++ src/main/java/com/hc/modules/course/controller/CourseController.java
@@ -0,0 +1,43 @@
+package com.hc.modules.course.controller;
+
+
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.hc.common.utils.ResponseUtil;
+import com.hc.modules.course.entity.CourseEntity;
+import com.hc.modules.course.service.CourseService;
+import com.hc.modules.teacher.entity.ClassEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+@RestController
+public class CourseController {
+
+    @Resource
+    private CourseService courseService;
+
+   /* *//**
+     * 新增
+     *//*
+    @RequestMapping(value = "/course/create", method = RequestMethod.POST)
+    public ResponseUtil createCourse(CourseEntity courseEntity, Integer cid){
+        courseService.insertCourse(courseEntity, cid);
+        return ResponseUtil.success();
+    }*/
+
+    /**
+     * 查询班级课节列表
+     */
+    @RequestMapping(value = "/class/course/list",method = RequestMethod.GET)
+    public ResponseUtil getClassList(Integer pageNo, Integer pageSize, Integer cid){
+        PageHelper.startPage(pageNo, pageSize);
+        List<CourseEntity> classList = courseService.getClassCourseList(cid);
+        PageInfo<CourseEntity> pageInfo = new PageInfo<>(classList);
+        return ResponseUtil.success(pageInfo);
+    }
+}
